3f164012ce93c79612cdf330aa8e34da84af85f9
Hübner <Kevin.Huebner@dm.de>
2016-09-28 15:28:10 +0200

    Setting up project with angular2

---
+++ app/app.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'kh-ng2-agenda',
+    templateUrl: 'component.html'
+})
+export class khNg2AgendaComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+}
